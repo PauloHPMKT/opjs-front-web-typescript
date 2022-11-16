1f82fad5af98e3abb7b21ca88e11f18f3f9ad1a9
Paulo Sergio <pauloserg861@gmail.com>
2022-11-16 20:08:52 -0300

    criando header da aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
 const App = () => {
   return(
 		<>
 			<GlobalStyles />
-			<h1>Hello world</h1>
+			<Header />
 		</>
   )
 }
