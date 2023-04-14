e139cae8c8cdf047aba12eb7d8b461c307eeac8a
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-13 22:06:20 -0300

    feat: create slide menu

---
+++ src/components/SlideMenu/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+const SlideMenu = () => {
+	return (
+		<Container>
+			<li>Link 1</li>
+			<li>Link 2</li>
+			<li>Link 3</li>
+			<li>Link 4</li>
+		</Container>
+	);
+}
+
+export default SlideMenu;
---
+++ src/components/SlideMenu/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.ul`
+	padding: 20px;
+	background-color: #fff;
+	box-shadow: rgba(0, 0, 0, 0.166);
+`
