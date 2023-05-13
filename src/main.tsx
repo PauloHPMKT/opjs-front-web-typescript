4da7d0b47012b2e876b72c9c606fddabd0574917
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-05-13 16:09:46 -0300

    feat: instanciando router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 //import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<App />
+	<Router>
+		<App />
+	</Router>
   /*<React.StrictMode>
   </React.StrictMode>*/
 )
