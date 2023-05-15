fa0a1e9ff95c84f2a606725821d2f7a46806aedf
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-05-15 20:40:34 -0300

    feat: implementando funçoes de login

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -1,19 +1,49 @@
+import { ChangeEvent, useContext, useState } from "react";
 import { Button } from "../Button";
 import { Form } from "./styles";
+import { AuthContext } from "../../contexts/Auth/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const FormLogin = () => {
-	const preventSendEvent = (e: MouseEvent) => {
-		e.preventDefault()
-		console.log(e);
+	const auth = useContext(AuthContext)
+	const navigate = useNavigate()
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-		alert('teste')
+	const handleEmailInput = (e: ChangeEvent<HTMLInputElement>) => {
+		setEmail(e.target.value)
+	}
+
+	const handlePasswordInput = (e: ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value)
+	}
+
+	const handleLogin = async () => {
+		if (email && password) {
+			const isLogged = await auth.signin(email, password);
+			if (isLogged) {
+				navigate('/')
+			} else {
+				alert('Login falhou!')
+			}
+		}
 	}
 
 	return (
 		<Form>
-			<input type="text" placeholder="email" />
-			<input type="password" placeholder="senha" />
-			<Button handleClickEvent={preventSendEvent}>
+			<input
+				type="text"
+				placeholder="email"
+				onChange={handleEmailInput}
+				value={email}
+			/>
+			<input
+				type="password"
+				placeholder="senha"
+				onChange={handlePasswordInput}
+				value={password}
+			/>
+			<Button handleClickEvent={handleLogin}>
 				Login
 			</Button>
 		</Form>
