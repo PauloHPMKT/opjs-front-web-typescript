54088b377330b4811fa92948e5a2631e87daafa1
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-05-14 22:33:20 -0300

    feat: criando browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,33 @@
-//import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css'
 
-import App from './App'
+import { App } from './App'
+import { LoginView } from './pages/LoginView';
+import { Dashboard } from './pages/Dashboard';
+import { ErrorPage } from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/",
+				element: <Dashboard />,
+			},
+			{
+				path: "login",
+				element: <LoginView />,
+			},
+		]
+	},
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<Router>
-		<App />
-	</Router>
-  /*<React.StrictMode>
-  </React.StrictMode>*/
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
